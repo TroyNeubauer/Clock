90dd67804496d4d5f6fc798a6a89f9f5b97e7537
Troy Neubauer <troyneubauer@gmail.com>
2018-08-27 15:05:10 -0400

    Got test led fade working

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,20 @@
 #include <Arduino.h>
 
-int led = 2;           // the PWM pin the LED is attached to
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int PIN = 2;           // the PWM pin the LED is attached to
+long start;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(PIN, OUTPUT);
+  start = millis();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  analogWrite(led, brightness);
-
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+    double inital = (millis() - start) / 1000.0;
+    double postSin = (sin(inital) + 1.0) / 2.0;
+    int value = (int)(postSin * 255.0);
+    analogWrite(PIN, value);
+    delay(30);
 }
