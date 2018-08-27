32c57412bf7f31fa5626736919d7bedf1eac185d
Troy Neubauer <troyneubauer@gmail.com>
2018-08-27 15:06:01 -0400

    Fixed formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <Arduino.h>
 
-int PIN = 2;           // the PWM pin the LED is attached to
+int PIN = 2;
 long start;
 
-// the setup routine runs once when you press reset:
+
 void setup() {
   // declare pin 9 to be an output:
   pinMode(PIN, OUTPUT);
   start = millis();
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
     double inital = (millis() - start) / 1000.0;
     double postSin = (sin(inital) + 1.0) / 2.0;
